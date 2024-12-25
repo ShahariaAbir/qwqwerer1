1401dbf76912ade6cafedd0f72cfde00edb2be94
ShahariaAbir <165765842+ShahariaAbir@users.noreply.github.com>
2024-12-25 20:12:15 +0600

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const config = {
     // Team capacity settings
     totalSeats: 100,
-    takenSeats: 25,
+    takenSeats: 33,
     
     // Join link
     joinLink: 'https://canvapro-three.vercel.app',
