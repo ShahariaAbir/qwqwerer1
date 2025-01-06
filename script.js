cced402ac0a7f8b3503edf44ef0babab7e1a3067
ShahariaAbir <165765842+ShahariaAbir@users.noreply.github.com>
2025-01-06 15:43:16 +0600

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const config = {
     // Team capacity settings
     totalSeats: 500,
-    takenSeats: 200,
+    takenSeats: 400,
     
     // Join link
     joinLink: 'https://nanolinks.in/pe1k7J2R',
