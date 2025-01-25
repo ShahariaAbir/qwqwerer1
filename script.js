c550b0e058545b380ec7a97a275cc9794ecca27b
ShahariaAbir <165765842+ShahariaAbir@users.noreply.github.com>
2025-01-25 13:22:07 +0600

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const config = {
     // Team capacity settings
     totalSeats: 500,
-    takenSeats: 10,
+    takenSeats: 300,
     
     // Join link
     joinLink: 'https://anvanvanv119014811190148can.netlify.app',
