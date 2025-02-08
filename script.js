42950f9014f4a17baebd35f2c2f7d8cfb2764133
ShahariaAbir <165765842+ShahariaAbir@users.noreply.github.com>
2025-02-08 14:30:11 +0600

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const config = {
     // Team capacity settings
     totalSeats: 500,
-    takenSeats: 200,
+    takenSeats: 300,
     
     // Join link
     joinLink: 'https://canvaproteamform.vercel.app',
