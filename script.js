9fd3e5936ada8fb9d5eb8122f831b06a3bafa33d
ShahariaAbir <165765842+ShahariaAbir@users.noreply.github.com>
2025-02-10 22:13:37 +0600

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const config = {
     takenSeats: 78,
     
     // Join link
-    joinLink: 'https://arolinks.com/bUGVS',
+    joinLink: 'https://arolinks.com/bdc',
     
     // Expiry date (YYYY-MM-DD format)
     expiryDate: '2025-2-15'
