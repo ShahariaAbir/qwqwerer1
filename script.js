548fa7b30b463558d9cd38b2d0f6b75f31ebc8a3
ShahariaAbir <165765842+ShahariaAbir@users.noreply.github.com>
2025-02-15 15:45:01 +0600

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const config = {
     takenSeats: 210,
     
     // Join link
-    joinLink: 'https://arolinks.com/tmCv',
+    joinLink: 'https://adrinolinks.in/jv4st',
     
     // Expiry date (YYYY-MM-DD format)
     expiryDate: '2025-2-15'
