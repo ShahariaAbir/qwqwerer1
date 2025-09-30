f46e6bfad1e4f95f868841a9a1eef7fbe3569b63
ShahariaAbir <165765842+ShahariaAbir@users.noreply.github.com>
2025-09-30 16:40:04 +0600

    Update script.js

--- script.js
+++ script.js
@@ -57,8 +57,8 @@ const configs = [
         id: 2,
         totalSeats: 500,
         takenSeats: 150,
-        joinLink: 'https://adrinolinks.com/CX3qkYj',
-        expiryDate: '2025-9-30'
+        joinLink: 'https://adrinolinks.com/c7ACJtT',
+        expiryDate: '2025-10-10'
     }
 ];
 
@@ -132,6 +132,7 @@ function updateSeats(id, taken) {
         updateContent();
     }
 } 
+
 
 
 
