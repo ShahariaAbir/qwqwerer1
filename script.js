0c85136cb4387f88d466ad1a036a51d099cc89df
ShahariaAbir <165765842+ShahariaAbir@users.noreply.github.com>
2025-11-15 17:05:13 +0600

    Fix join link for configuration ID 2
    Updated join link for configuration ID 2.

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ const configs = [
         id: 2,
         totalSeats: 500,
         takenSeats: 150,
-        joinLink: 'https://is.gd/54c4Eq',
+        joinLink: 'https://adrinolinks.com/axEbq',
         expiryDate: '2025-10-30'
     }
 ];
@@ -132,6 +132,7 @@ function updateSeats(id, taken) {
         updateContent();
     }
 } 
+
 
 
 
